8e18b48cd7fba55284ac5b6ad9c113c8393fbba5
kysko <ziomus5555@gmail.com>
2017-06-21 12:12:32 +0200

    Dodaj pliki projektów.

---
+++ sieciServ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sieciServ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
